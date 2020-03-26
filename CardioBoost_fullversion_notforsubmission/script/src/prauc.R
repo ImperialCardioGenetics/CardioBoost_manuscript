74de6f79483ecdb9a0aa174b6e101a28d5302768
XiaoleiZ <xiaolei.zhang@imperial.ac.uk>
2020-03-26 01:02:38 +0000

    delete all folders

--- CardioBoost_fullversion_notforsubmission/script/src/prauc.R
+++
@@ -1,8 +0,0 @@
-#calculate PRAUC given a mlr object
-library(PRROC)
-prauc=function(task,model,pred,feats,extra.args){
-  prob_positive<-(1-getPredictionProbabilities(pred))[pred$data$truth==1]
-  prob_negative<-(1-getPredictionProbabilities(pred))[pred$data$truth==0]
-  pr <- pr.curve(prob_positive,prob_negative,  curve = F)
-  pr$auc.integral
-}
--- CardioBoost_submission/script/install_requiredRpkg.R
+++
@@ -1,5 +0,0 @@
-require(devtools)
-pkg_list=read.table("./requiredRpkgs.txt",stringsAsFactors=FALSE,sep="_")
-for (i in 1:nrow(pkg_list)){
-install_version(package = pkg_list[i,1],version=pkg_list[i,2],repos = "http://cran.us.r-project.org")
-}
